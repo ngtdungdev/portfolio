e639d674871c214ea068c83f9b11b6640870a674
Tiến Dũng <dungboi1029@gmail.com>
2026-06-25 12:23:04 +0700

    new

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
+// Node modules
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// CSS link
 import './index.css'
+// Components
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
