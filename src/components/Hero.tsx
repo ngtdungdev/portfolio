fc778c2311cce0950f92909e187230ed2b0de001
Tiến Dũng <dungboi1029@gmail.com>
2026-06-27 19:51:17 +0700

    new

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,43 @@
 const Hero = () => {
     return (
-        <section id="home" className="">
-            <div className="">
+        <section id="home" className="pt-28 lg:pt-36">
+            <div className="container items-center lg:grid lg:grid-cols-2 lg:gap-10">
                 <div>
-                    <div className="">
-                        <figure className="">
+                    <div className="flex items-center gap-3">
+                        <figure className="img-box w-9 h-9 rounded-lg">
                             <img src="/images/avatar-1.jpg"
                                  width={40}
                                  height={40}
                                  alt="Nguyen Tien Dung portrait"
                                  className="img-cover"/>
                         </figure>
-                        <div className="">
-                            <span className="">
-                                <span className=""></span>
+                        <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                            <span className="relative w-2 h-2 rounded-full bg-emerald-400">
+                                <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping">
+
+                                </span>
                             </span>
                             Available for work
                         </div>
-                        <h2 className="">
-                            Building Scalable Modern Websites for the Future
-                        </h2>
-                        <div className="">
-                            ButtonPrimary
+                    </div>
+                    <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+                        Building Scalable Modern
+                        Websites for the Future
+                    </h2>
+                    <div className="flex items-center gap-3">
+                        ButtonPrimary
 
-                            ButtonOutline
-                        </div>
+                        ButtonOutline
                     </div>
                 </div>
-                <div className="">
-                    <figure className="">
-                        <img src="/images/hero-banner.png" width={656} height={800} alt="Nguyen Tien Dung"
-                             className=""/>
+                <div className="hidden lg:block">
+                    <figure
+                        className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+                        <img src="/images/hero-banner.png"
+                             width={656}
+                             height={800}
+                             alt="Nguyen Tien Dung"
+                             className="w-full"/>
                     </figure>
                 </div>
             </div>
